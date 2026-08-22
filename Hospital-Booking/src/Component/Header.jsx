4a71d5a357da816f92522a18fba9aa5f800715ad
Mohiuddin ansari <mohiuddinansari04@gmail.com>
2026-08-22 17:15:50 +0530

    navbar

--- Hospital-Booking/src/Component/Header.jsx
+++ Hospital-Booking/src/Component/Header.jsx
@@ -3,10 +3,10 @@ import React from 'react'
 function Header() {
   return (
     <div className="bg-blue-500 text-white p-4 flex justify-between items-center">
-      <h1>Hospital Booking</h1>
-      <div>
-        <nav>
-          <ul>
+      <h1 className="text-3xl font-bold">Hospital Booking</h1>
+      <div className="flex items-center" >
+        <nav className="flex items-center gap-4">
+          <ul className="flex flex-col sm:flex-row gap-4">
             <li><a href="/">Home</a></li>
             <li><a href="/about">About</a></li>
             <li><a href="/contact">Contact</a></li>
