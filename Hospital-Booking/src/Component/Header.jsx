0bfea2080ecf34c98b8b889b266118b4a7dbbfe6
Mohiuddin ansari <mohiuddinansari04@gmail.com>
2026-08-25 17:09:06 +0530

    mincer changes

--- Hospital-Booking/src/Component/Header.jsx
+++ Hospital-Booking/src/Component/Header.jsx
@@ -1,15 +1,18 @@
 import React from 'react'
 
 function Header() {
+  const arr=["Home","About","Contact"]
   return (
     <div className="bg-blue-500 text-white p-4 flex justify-between items-center">
       <h1 className="text-3xl font-bold text-green-300 flex ">Day-Care-Hospital</h1>
       <div className="flex items-center" >
         <nav className="flex items-center gap-4">
           <ul className="flex flex-col sm:flex-row gap-4">
-            <li className="hover:underline"><a href="/">Home</a></li>
-            <li className="hover:underline"><a href="/about">About</a></li>
-            <li className="hover:underline"><a href="/contact">Contact</a></li>
+            {arr.map((item) => (
+              <li key={item} className="hover:underline">
+                <a href={`/${item.toLowerCase()}`}>{item}</a>
+              </li>
+            ))}
           </ul>
         </nav>
       </div>
