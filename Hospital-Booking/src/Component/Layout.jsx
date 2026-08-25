1c82dac39dcc83bf19ff0ad9ae16a7e7d7e44bb5
Mohiuddin ansari <mohiuddinansari04@gmail.com>
2026-08-25 17:29:23 +0530

    changes in the layout

--- Hospital-Booking/src/Component/Layout.jsx
+++ Hospital-Booking/src/Component/Layout.jsx
@@ -2,11 +2,18 @@ import React from 'react'
 
 function Layout() {
   return (
-    <div>
-      <h1>Welcome to the Hospital Booking App</h1>
-      <p>This is the main content area.</p>
+    <div className="p-4 bg-gray-200 min-h-screen flex flex-col  justify-center items-center">
+
+      <h1 className="text-3xl font-bold">Welcome to the Hospital Booking App</h1>
+
+      <p className=" p-5 text-xl">Day Care Appointments
+
+Get convenient and reliable day care services without the need for an overnight hospital stay. Book appointments with experienced doctors and healthcare professionals for consultations, minor procedures, follow-up care, and other same-day treatments. Choose a suitable time, find the right specialist, and manage your appointment easily—all in one place.</p>
+       
+        <img className='w-auto' src="https://png.pngtree.com/thumb_back/fh260/background/20240419/pngtree-smiling-doctor-or-consultant-sitting-at-a-desk-his-neck-looking-image_15663381.jpg" alt="" />
     </div>
+      
   )
 }
 
-export default Layout
+export default Layout;
