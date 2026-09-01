3aae918efbe98ca7a5821a529590c4039d0b5b25
Mohiuddin ansari <mohiuddinansari04@gmail.com>
2026-09-01 17:13:11 +0530

    header changes

--- Hospital-Booking/src/Component/Header.jsx
+++ Hospital-Booking/src/Component/Header.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import { FaHospitalUser } from "react-icons/fa";
 
 function Header() {
   const arr=["Home","About","Contact"]
   return (
     <div className="bg-blue-500 text-white p-4 flex justify-between items-center">
-      <h1 className="text-3xl font-bold text-green-300 flex ">Day-Care-Hospital</h1>
+      <h1 className="text-3xl font-bold flex "><FaHospitalUser className="mr-2 mt-1" /> Hospital-Booking </h1>
       <div className="flex items-center" >
         <nav className="flex items-center gap-4">
           <ul className="flex flex-col sm:flex-row gap-4">
